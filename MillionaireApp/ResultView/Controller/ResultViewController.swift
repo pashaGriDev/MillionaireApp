36fc42bb1e5153f2e557094879a8cbfe330b0ac6
Sivertsev Maxim <sivertsev1995@icloud.com>
2024-02-29 18:03:21 +0300

    2

--- MillionaireApp/ResultView/Controller/ResultViewController.swift
+++ MillionaireApp/ResultView/Controller/ResultViewController.swift
@@ -31,7 +31,6 @@ class ResultViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        view.addSubview(customView)
         customView.setupLayots()
         setupLayots()
     }
