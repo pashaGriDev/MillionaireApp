9820dd8478dbc2a5fb407f4a5159a2a7424c93ab
pashaGriDev <104725088+pashaGriDev@users.noreply.github.com>
2024-03-02 19:12:17 +0400

    update(gameScreen): new arrangement of elements and corrected errors

--- MillionaireApp/UI/GamePage/GameViewController.swift
+++ MillionaireApp/UI/GamePage/GameViewController.swift
@@ -10,15 +10,20 @@ final class GameViewController: UIViewController {
     private var questionNumber = 0
     private let parser = JsonParser()
     
-    private let backgroundImageView = UIImageView.commonImageView(imageName: "gameBg")
-    private let logoImageView = UIImageView.commonImageView(imageName: "imageLogo")
+    private let backgroundImageView = UIImageView(image: UIImage(named: "firstBackgroundImage"))
+    
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "imageLogo"))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
     
     private let questionLabel: UILabel = {
         let label = UILabel()
         label.font = .preferredFont(forTextStyle: .title3)
         label.numberOfLines = .zero
         label.textColor = .white
-        label.textAlignment = .left
+        label.textAlignment = .center
         return label
     }()
     
@@ -34,7 +39,7 @@ final class GameViewController: UIViewController {
         let label = UILabel()
         label.font = .preferredFont(forTextStyle: .title1)
         label.textColor = .white
-        label.textAlignment = .left
+        label.textAlignment = .right
         return label
     }()
     
@@ -47,32 +52,12 @@ final class GameViewController: UIViewController {
     private let cluePhoneButton =  ClueButton(imageName: "cluePhone")
     private let clue50Button =  ClueButton(imageName:  "clue50")
     
-    private lazy var firstLineStack: UIStackView = {
-       let stack = UIStackView()
-        stack.alignment = .leading
-        stack.axis = .horizontal
-        stack.spacing = 40
-        stack.addArrangedSubview(logoImageView)
-        stack.addArrangedSubview(questionLabel)
-        return stack
-    }()
-    
     private lazy var secondLineStack: UIStackView = {
        let stack = UIStackView()
-        stack.alignment = .center
-        stack.axis = .horizontal
-        stack.spacing = 100
+        stack.spacing = 10
+        stack.distribution = .fillEqually
         stack.addArrangedSubview(questionNumberLabel)
         stack.addArrangedSubview(priceLabel)
-        return stack
-    }()
-    private lazy var informStack: UIStackView = {
-       let stack = UIStackView()
-        stack.alignment = .leading
-        stack.axis = .vertical
-        stack.spacing = 20
-        stack.addArrangedSubview(firstLineStack)
-        stack.addArrangedSubview(secondLineStack)
         return stack
     }()
     
@@ -80,7 +65,7 @@ final class GameViewController: UIViewController {
        let stack = UIStackView()
         stack.alignment = .center
         stack.axis = .vertical
-        stack.spacing = 50
+        stack.spacing = 10
         stack.addArrangedSubview(buttonA)
         stack.addArrangedSubview(buttonB)
         stack.addArrangedSubview(buttonC)
@@ -103,7 +88,7 @@ final class GameViewController: UIViewController {
        let stack = UIStackView()
         stack.alignment = .center
         stack.axis = .vertical
-        stack.spacing = 100
+        stack.spacing = 50
         stack.addArrangedSubview(questionVerticalStack)
         stack.addArrangedSubview(clueStackView)
         return stack
@@ -111,6 +96,7 @@ final class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.isNavigationBarHidden = true
         setup()
         getData()
     }
@@ -123,26 +109,36 @@ final class GameViewController: UIViewController {
     }
     
     private func setup() {
-        view.addSubviews([backgroundImageView, informStack, mainVerticalStack])
+        view.addSubviews([
+            backgroundImageView,logoImageView, questionLabel, secondLineStack, mainVerticalStack
+        ])
         
         backgroundImageView.snp.makeConstraints {
-            $0.bottom.equalToSuperview()
-            $0.trailing.leading.equalToSuperview()
-            $0.height.equalTo(200)
+            $0.edges.equalToSuperview()
         }
-        logoImageView.snp.makeConstraints {
-            $0.height.equalTo(87)
-            $0.width.equalTo(87)
+        
+        logoImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.centerX.equalToSuperview()
+            make.height.width.equalTo(50)
         }
-        informStack.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.leading.equalToSuperview().offset(20)
-            $0.trailing.equalToSuperview().inset(16)
-            $0.top.equalTo(70)
+        
+        questionLabel.snp.makeConstraints { make in
+            make.top.equalTo(logoImageView.snp.bottom).offset(10)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.greaterThanOrEqualTo(150).priority(.high)
         }
+        
+        secondLineStack.snp.makeConstraints { make in
+            make.top.equalTo(questionLabel.snp.bottom).offset(10)
+            make.height.equalTo(100)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+        
         mainVerticalStack.snp.makeConstraints {
-            $0.top.equalTo(informStack.snp.bottom).offset(30)
-            $0.trailing.leading.equalTo(10)
+            $0.top.equalTo(secondLineStack.snp.bottom).offset(30)
+            $0.trailing.leading.equalToSuperview().inset(10)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-10)
         }
     }
     
